8bf3695c3809e27581046443a67d15dfbc17df8f
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2020-12-26 01:44:28 +0900

    [feat]make new router only give 9 news and fix router to match with new mongo db coleection form

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,11 @@ async def custom_http_exception_handler(request, exc):
     return RedirectResponse("/")
 #에러뜰경우 root로
 
-#go
+# @app.get("/")
+
+
+
+
 @app.get("/cate/live/")
 async def cate_live(datetime: str, cate: str="hot"):
     db = client['Real_time_Result']
@@ -41,103 +45,119 @@ async def cate_live(datetime: str, cate: str="hot"):
     item = mycol.find_one({},{cate:1})
     result=[]
     for a in range(len(item[cate])):
-        i=a+1
         cur={}
         main_id=item[cate][str(i)]["id_list"][0]
         db=client['Info']
-        main_news=db["Info"].find_one({},{main_id:1})
+        main_news=db["Info"].find({"id":main_id})[0]
         cur['cluster_num']=i
-        cur["img_url"]=main_news[main_id]["img_url"]
-        cur["title"]=main_news[main_id]["title"]
-        cur["url"]=main_news[main_id]["url"]
-        cur["press"]=main_news[main_id]["press"]
+        cur["img_url"]=main_news["img_url"]
+        cur["title"]=main_news["title"]
+        cur["url"]=main_news["url"]
+        cur["press"]=main_news["press"]
         cur["keyword"]=item[cate][str(i)]["keyword_list"]
         cur["title_list"]=[]
         for now_id in item[cate][str(i)]["id_list"]:
             print(now_id)
             now_dic={}
-            now_news=db["Info"].find_one({},{now_id:1})
-            now_dic["title"]=now_news[now_id]["title"]
-            now_dic["url"]=now_news[now_id]["url"]
-            now_dic["press"]=now_news[now_id]["press"]
+            now_news=db["Info"].find({"id":now_id})
+            now_dic["title"]=now_news["title"]
+            now_dic["url"]=now_news["url"]
+            now_dic["press"]=now_news["press"]
             cur["title_list"].append(now_dic)
         result.append(cur)
     return result
-#end
-# @app.get("/cate/live/")
-# async def cate_live(datetime:str,cate:str="hot"):
-#     print("first_route")
-#     result=[]
-#     item=Real_time_Result.datetime.find({},{cate:true})
-#     a=list(item[cate].keys())
-#     a.sort()
-#     for i in a:
-#         cur={}
-#         main_id=item[cate][i]["id_list"][0]
-#         main_news=Info.Info.find({},{main_id:true})
-#         cur['cluster_num']=i
-#         cur["image_url"]=main_news[main_id]["image_url"]
-#         cur["title"]=main_news[main_id]["title"]
-#         cur["url"]=main_news[main_id]["url"]
-#         cur["press"]=main_news[main_id]["press"]
-#         cur["keyword_list"]=item[cate][i]["id_list"]
-#         cur["show"]='wait'
-#         cur["title_list"]=[]
-#         for now_id in item[cate][i]["id_list"]:
-#             now_dic={}
-#             now_news=Info.Info.find({},{now_id:true})
-#             now_dic["title"].append(now_news[now_id]["title"])
-#             now_dic["url"].append(now_news[now_id]["url"])
-#             now_dic["press"].append(now_news[now_id]["press"])
-#             cur["title_list"].append(now_dic)
-#         result.append(cur)
-#     return result
+
+@app.get("/cate/live_nine/")
+async def cate_live(datetime:str,cate:str="hot"):
+    db=client['Real_time_Result']
+    mycol=db[datetime]
+    item=mycol.find_one({},{cate:1})
+    result=[]
+    num=0
+    for a in range(len(item[cate])):
+        num=num+1
+        if num==10:
+            break
+        cur={}
+        main_id=item[cate][str(i)]["id_list"][0]
+        db=client['Info']
+        main_news=db["Info"].find({"id":main_id})[0]
+        cur['cluster_num']=i
+        cur["img_url"]=main_news["img_url"]
+        cur["title"]=main_news["title"]
+        cur["url"]=main_news["url"]
+        cur["press"]=main_news["press"]
+        cur["keyword"]=item[cate][str(i)]["keyword_list"]
+        cur["title_list"]=[]
+        for now_id in item[cate][str(i)]["id_list"]:
+            print(now_id)
+            now_dic={}
+            now_news=db["Info"].find({"id":now_id})
+            now_dic["title"]=now_news["title"]
+            now_dic["url"]=now_news["url"]
+            now_dic["press"]=now_news["press"]
+            cur["title_list"].append(now_dic)
+        result.append(cur)
+    return result
 
 @app.get("/cate/day/")
 async def cate_day(datetime:str,cate:str="hot"):
-    db=client['Day_Result']
+    db=client['Real_time_Result']
     mycol=db[datetime]
     item=mycol.find_one({},{cate:1})
     result=[]
     for a in range(len(item[cate])):
-        i=a+1
         cur={}
         main_id=item[cate][str(i)]["id_list"][0]
         db=client['Info']
-        main_news=db["Info"].find_one({},{main_id:1})
+        main_news=db["Info"].find({"id":main_id})[0]
         cur['cluster_num']=i
-        cur["img_url"]=main_news[main_id]["img_url"]
-        cur["title"]=main_news[main_id]["title"]
-        cur["url"]=main_news[main_id]["url"]
-        cur["press"]=main_news[main_id]["press"]
+        cur["img_url"]=main_news["img_url"]
+        cur["title"]=main_news["title"]
+        cur["url"]=main_news["url"]
+        cur["press"]=main_news["press"]
         cur["keyword"]=item[cate][str(i)]["keyword_list"]
         cur["title_list"]=[]
         for now_id in item[cate][str(i)]["id_list"]:
             print(now_id)
             now_dic={}
-            now_news=db["Info"].find_one({},{now_id:1})
-            now_dic["title"]=now_news[now_id]["title"]
-            now_dic["url"]=now_news[now_id]["url"]
-            now_dic["press"]=now_news[now_id]["press"]
+            now_news=db["Info"].find({"id":now_id})
+            now_dic["title"]=now_news["title"]
+            now_dic["url"]=now_news["url"]
+            now_dic["press"]=now_news["press"]
             cur["title_list"].append(now_dic)
         result.append(cur)
     return result
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         "main.app", 
-#         host="localhost", 
-#         port=8000, 
-#         reload=True)
 
-# class front_in(BaseModel):
-#     datetime: str
-#     cate: str
-
-# class front_out(BaseModel):
-#     cluster_num: int
-#     image_url: str
-#     title: str
-#     url: str
-#     press: str
-#     keyword: list[str]
-#     title_list: list[dict(str, str, str)]
+@app.get("/cate/day_nine/")
+async def cate_day(datetime:str,cate:str="hot"):
+    db=client['Real_time_Result']
+    mycol=db[datetime]
+    item=mycol.find_one({},{cate:1})
+    result=[]
+    num=0
+    for a in range(len(item[cate])):
+        num=num+1
+        if num==10:
+            break
+        cur={}
+        main_id=item[cate][str(i)]["id_list"][0]
+        db=client['Info']
+        main_news=db["Info"].find({"id":main_id})[0]
+        cur['cluster_num']=i
+        cur["img_url"]=main_news["img_url"]
+        cur["title"]=main_news["title"]
+        cur["url"]=main_news["url"]
+        cur["press"]=main_news["press"]
+        cur["keyword"]=item[cate][str(i)]["keyword_list"]
+        cur["title_list"]=[]
+        for now_id in item[cate][str(i)]["id_list"]:
+            print(now_id)
+            now_dic={}
+            now_news=db["Info"].find({"id":now_id})
+            now_dic["title"]=now_news["title"]
+            now_dic["url"]=now_news["url"]
+            now_dic["press"]=now_news["press"]
+            cur["title_list"].append(now_dic)
+        result.append(cur)
+    return result
